de23f1d4f34abc021f68d93b5679880916dcb2fc
Joe <joe.friedrich@gmail.com>
2018-07-26 21:12:06 -0400

    Added comments to buffer command line logic and doc strings to functions.

--- answer3/next_pyco_monday.py
+++ answer3/next_pyco_monday.py
@@ -3,15 +3,27 @@ import sys
 import getopt
 
 def is_leap_year(year):
+    '''
+        Tests 'year' to verify if it is a leap year.
+        Returns True or False.
+    '''
     return (year % 4 == 0 and year % 100 != 0) or year % 400 == 0
 
 def next_leap_year(start_year):
+    '''
+        From 'start_year', calculate the next leap year.
+        Returns the next leap year.
+    '''
     year = start_year
     while not is_leap_year(year):
         year += 1
     return year
 
 def feb_29_is_monday(year):
+    '''
+        For given 'year' determines that Feb. 29 is a Monday.
+        Returns True or False.
+    '''
     d = datetime.date(year, 2, 29)
     if d.weekday() == 0:
         return True
@@ -19,6 +31,12 @@ def feb_29_is_monday(year):
         return False
 
 def next_pyco_leapyear_monday(start_year):
+    '''
+        Passed integer 'start_year'.
+        Sets year to be the leap year after 'start year'.
+        While loop tests leap day to verify it is a Monday.
+        Returns Leap year that COHPY meets on a leap day. 
+    '''
     year = next_leap_year(start_year)
     while not feb_29_is_monday(year):
         year = next_leap_year(year + 1)
@@ -26,6 +44,8 @@ def next_pyco_leapyear_monday(start_year):
 
 def main(args):
     year = None
+    
+    #---Start command line options logic---
     try:
         opts, args = getopt.getopt(
             args[1:],
@@ -43,6 +63,8 @@ def main(args):
     if not year:
         print 'python next_pyco_monday.py -y <start_year>'
         sys.exit(2)
+    #---End command line options logic---
+    
     print 'Starting is %d' % year
     print (("Next year with a Central Ohio Python User Group meetup "
         "on a leapyear end of February: %d") %
